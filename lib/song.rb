7347edd4dd397561d2240f1bf25129628a530251
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-10 03:37:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
 
   def self.genre_count
-    @@genres.map{ |k, v| [k=>v.size] }
+    @@genres.group_by{ |v| v }.map{ |k, v| [k=>v.size] }
     # @@genres.map do |k, v|
     #   v = 0
     #   if k == v
