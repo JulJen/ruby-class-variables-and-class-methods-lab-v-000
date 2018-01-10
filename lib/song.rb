084004fe030dc70e89ad9e6d6a2652594dcf6bc1
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-10 03:44:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
 
   def self.genre_count
     # @@genres.group_by{ |v| v }.map{ |k, v| [k => v.size] }
-    @@genres.inject(Hash.new(0) { |hash, key| hash[key] = +=1 })
+    @@genres.inject(Hash.new(0) { |hash, key| hash[key] = +=1 }
   end
 
   def self.artist_count
