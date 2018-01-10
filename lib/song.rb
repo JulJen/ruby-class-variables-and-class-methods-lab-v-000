d8a0a83520b93b3093ac0f38e5320271c0cb4de7
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-10 02:48:32 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist, :genre
+  # attr_reader :name, :artist, :genre
+
+  # @@artist_count
+  # @@genre_count
+  @@artists = []
+  @@genres = []
+  @@count = 0
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genre_count
+  end
+
+  def self.artist_count
+  end
+
+
+end
+
+
+
+  #   @@genres = value
+  # end
+
+
+  # def self.count=(value)
+  #   @@count = value
+  # end
